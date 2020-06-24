bc53c92e9841ae7d00e34826107ed1962b7eb3f4
José Roberto <jrpradojunior@gmail.com>
2020-06-23 23:46:57 -0300

    first commit

---
+++ _data.tf
@@ -0,0 +1,21 @@
+data "aws_region" "current" {}
+data "aws_ami" "amzn_windows" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Windows_Server-2016-English-Full-ECS_Optimized*"]
+  }
+
+  name_regex = ".+-ECS_Optimized$"
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_iam_account_alias" "current" {}
+
+#-------
+# KMS
+data "aws_kms_key" "ebs" {
+  key_id = "alias/aws/ebs"
+}
