e8aef45fd48307ed5e74f5c48928ebf847de17f1
rjlmota <rjlmota@gmail.com>
2021-09-30 10:32:14 -0300

    adding fsx

---
+++ fsx.tf
@@ -0,0 +1,38 @@
+resource "aws_fsx_windows_file_system" "ecs" {
+  active_directory_id = aws_directory_service_directory.example.id
+  kms_key_id          = aws_kms_key.example.arn
+  storage_capacity    = 300
+  subnet_ids          = [aws_subnet.example.id]
+  throughput_capacity = 1024
+
+
+  tags = {
+    Name   = "ecs-${var.name}"
+    Backup = var.backup
+  }
+
+}
+
+
+
+resource "aws_security_group" "fsx" {
+  count       = var.create_fsx ? 1 : 0
+  name        = "ecs-${var.name}-efs"
+  description = "for EFS to talk to ECS cluster"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "ecs-fsx-${var.name}"
+  }
+}
+
+resource "aws_security_group_rule" "nfs_from_ecs_to_efs" {
+  count                    = var.create_fsx ? 1 : 0
+  description              = "ECS to FSX"
+  type                     = "ingress"
+  from_port                = 2049
+  to_port                  = 2049
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.fsx[0].id
+  source_security_group_id = aws_security_group.ecs_nodes.id
+}
