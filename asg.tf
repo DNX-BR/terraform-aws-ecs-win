766a639a2cccd3416aec9a99373c5b4d0ce7d8ec
rjlmota <rjlmota@gmail.com>
2021-10-04 14:31:32 -0300

    correct asg capacity provider name

--- asg.tf
+++ asg.tf
@@ -47,7 +47,7 @@ resource "aws_autoscaling_group" "ecs" {
 
 
 resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
-  name  = "${var.name}-capacity-provider"
+  name  = "${var.name}-win-capacity-provider"
 
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.ecs.arn
