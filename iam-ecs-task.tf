d60c63d18aac8bdb7f97cc33e914711d59d56d8f
rjlmota <rjlmota@gmail.com>
2021-10-04 20:53:37 -0300

    new policy

--- iam-ecs-task.tf
+++ iam-ecs-task.tf
@@ -1,7 +1,36 @@
 resource "aws_iam_role" "ecs_task" {
   count = var.create_iam_roles ? 1 : 0
   name = "ecs-task-${var.name}-${data.aws_region.current.name}"
-  
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task" {
+  count = var.create_iam_roles ? 1 : 0
+  role       = aws_iam_role.ecs_task[0].name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy" "ssm_policy" {
+  count = var.create_iam_roles ? 1 : 0
+  name = "ecs-ssm-policy"
+  role = aws_iam_role.ecs_task[0].name
+
+
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -27,35 +56,6 @@ resource "aws_iam_role" "ecs_task" {
       ],
       "Resource": [
         "*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task" {
-  count = var.create_iam_roles ? 1 : 0
-  role       = aws_iam_role.ecs_task[0].name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_iam_role_policy" "ssm_policy" {
-  count = var.create_iam_roles ? 1 : 0
-  name = "ecs-ssm-policy"
-  role = aws_iam_role.ecs_task[0].name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ssm:GetParameters"
-      ],
-      "Resource": [
-        "arn:aws:ssm:*:*:parameter/*"
       ]
     }
   ]
