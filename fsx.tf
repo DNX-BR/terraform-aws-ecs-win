c51494875e94f90118bdc29583e0ee614bbc5532
rjlmota <rjlmota@gmail.com>
2021-10-05 16:45:55 -0300

    throughput too low

--- fsx.tf
+++ fsx.tf
@@ -3,7 +3,7 @@ resource "aws_fsx_windows_file_system" "ecs" {
   kms_key_id     = var.kms_key_arn != "" ? var.kms_key_arn : null
   storage_capacity    = 32
   subnet_ids      = list(element(var.secure_subnet_ids, count.index))
-  throughput_capacity = 1
+  throughput_capacity = 8
 
 
   security_group_ids = aws_security_group.fsx[0].id
