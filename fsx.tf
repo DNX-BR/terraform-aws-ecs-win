aab306211a6ddf6e9feb12ca8d7ff618631e6c33
rjlmota <rjlmota@gmail.com>
2021-10-05 10:32:10 -0300

    sg fsx egress rule

--- fsx.tf
+++ fsx.tf
@@ -21,12 +21,10 @@ resource "aws_fsx_windows_file_system" "ecs" {
 
 }
 
-
-
 resource "aws_security_group" "fsx" {
   count       = var.create_fsx ? 1 : 0
-  name        = "ecs-${var.name}-efs"
-  description = "for EFS to talk to ECS cluster"
+  name        = "ecs-${var.name}-fsx"
+  description = "for FSx to talk to ECS cluster"
   vpc_id      = var.vpc_id
 
   tags = {
@@ -34,7 +32,7 @@ resource "aws_security_group" "fsx" {
   }
 }
 
-resource "aws_security_group_rule" "fsx_from_ecs_to_efs" {
+resource "aws_security_group_rule" "fsx_from_ecs_to_fsx" {
   count                    = var.create_fsx ? 1 : 0
   description              = "ECS to FSX"
   type                     = "ingress"
@@ -43,4 +41,15 @@ resource "aws_security_group_rule" "fsx_from_ecs_to_efs" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.fsx[0].id
   source_security_group_id = aws_security_group.ecs_nodes.id
+}
+
+resource "aws_security_group_rule" "fsx_from_fsx_to_world" {
+  count                    = var.create_fsx ? 1 : 0
+  description              = "FSX to World"
+  type                     = "egress"
+  from_port                = -1
+  to_port                  = -1
+  protocol                 = "all"
+  security_group_id        = aws_security_group.fsx[0].id
+  source_security_group_id = ["0.0.0.0/0"]
 }
