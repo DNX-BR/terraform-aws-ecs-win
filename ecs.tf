b9836fa7b912cee188c305da5de2ad42f6c23bb1
rjlmota <rjlmota@gmail.com>
2021-10-06 12:57:55 -0300

    adding capacity provider

--- ecs.tf
+++ ecs.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_cluster" "ecs" {
   name = "${var.name}"
 
-  capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider[0].name]
+  capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
 }
